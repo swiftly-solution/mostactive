8a6eeab2a49ff0d6125da47f79a05db6677204c9
blu <tiganinpungasrl@gmail.com>
2025-02-23 13:41:29 +0200

    fix(mostactive): Fix spelling and hours formatting

--- plugins/mostactive/commands.lua
+++ plugins/mostactive/commands.lua
@@ -22,10 +22,10 @@ function HoursCommand(playerid, args, argc, silent, prefix)
         end
 
         ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "-------------------------------------")
-        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Hours CT: {red}" .. ComputePrettyTime(hours_ct))
-        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Hours T: {red}" .. ComputePrettyTime(hours_t))
-        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Hours SPEC: {red}" .. ComputePrettyTime(hours_spec))
-        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Total Hours: {red}" .. ComputePrettyTime(hours_total))
+        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Hours CT: {red}" .. formatTime(hours_ct))
+        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Hours T: {red}" .. formatTime(hours_t))
+        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Hours SPEC: {red}" .. formatTime(hours_spec))
+        ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "Total Hours: {red}" .. formatTime(hours_total))
         ReplyToCommand(playerid, config:Fetch("mostactive.prefix"), "-------------------------------------")
     end)
 end
@@ -45,14 +45,14 @@ commands:Register("tophours", function (playerid, args, argsCount, silent, prefi
     if option == "1" then
         db:QueryBuilder():Table("mostactive"):Select({"hours_ct", "name"}):OrderBy({ hours_ct = "DESC" }):Limit(10):Execute(function (err, result)
             if #err > 0 then
-                print("{RED} ERROR: ".. eerr)
+                print("{RED} ERROR: ".. err)
             end
             local top_ct = {}
             for i = 1, #result do
                 if (type(result[i])) == "table" then
                     local name = result[i]["name"]
                     local ore = tonumber(result[i]["hours_ct"] or 0)
-                    local ore_formatted = ComputePrettyTime(ore)
+                    local ore_formatted = formatTime(ore)
                     table.insert(top_ct, { name .. " - " .. ore_formatted.."", "" })
                 end
             end
@@ -63,14 +63,14 @@ commands:Register("tophours", function (playerid, args, argsCount, silent, prefi
     elseif option == "2" then
         db:QueryBuilder():Table("mostactive"):Select({"hours_t", "name"}):OrderBy({ hours_t = "DESC" }):Limit(10):Execute(function (err, result)
             if #err > 0 then
-                print("{RED} ERROR: ".. eerr)
+                print("{RED} ERROR: ".. err)
             end
             local top_t = {}
             for i = 1, #result do
                 if (type(result[i])) == "table" then
                     local name = result[i]["name"]
                     local ore = tonumber(result[i]["hours_t"] or 0)
-                    local ore_formatted = ComputePrettyTime(ore)
+                    local ore_formatted = formatTime(ore)
                     table.insert(top_t, { name .. " - " .. ore_formatted.."", "" })
                 end
             end
@@ -81,14 +81,14 @@ commands:Register("tophours", function (playerid, args, argsCount, silent, prefi
     elseif option == "3" then
         db:QueryBuilder():Table("mostactive"):Select({"hours_spec", "name"}):OrderBy({ hours_spec = "DESC" }):Limit(10):Execute(function (err, result)
             if #err > 0 then
-                print("{RED} ERROR: ".. eerr)
+                print("{RED} ERROR: ".. err)
             end
             local top_spec = {}
             for i = 1, #result do
                 if (type(result[i])) == "table" then
                     local name = result[i]["name"]
                     local ore = tonumber(result[i]["hours_spec"] or 0)
-                    local ore_formatted = ComputePrettyTime(ore)
+                    local ore_formatted = formatTime(ore)
                     table.insert(top_spec, { name .. " - " .. ore_formatted.."", "" })
                 end
             end
@@ -99,14 +99,14 @@ commands:Register("tophours", function (playerid, args, argsCount, silent, prefi
     elseif option == "4" then
         db:QueryBuilder():Table("mostactive"):Select({"hours_total", "name"}):OrderBy({ hours_total = "DESC" }):Limit(10):Execute(function (err, result)
             if #err > 0 then
-                print("{RED} ERROR: ".. eerr)
+                print("{RED} ERROR: ".. err)
             end
             local top_total = {}
             for i = 1, #result do
                 if (type(result[i])) == "table" then
                     local name = result[i]["name"]
                     local ore = tonumber(result[i]["hours_total"] or 0)
-                    local ore_formatted = ComputePrettyTime(ore)
+                    local ore_formatted = formatTime(ore)
                     table.insert(top_total, { name .. " - " .. ore_formatted.."", "" })
                 end
             end
